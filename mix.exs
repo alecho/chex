80f21bd42c15cd870546e48316495b6f3c06c0a7
Andrew Lechowicz <andrew@alecho.com>
2019-09-11 22:34:28 -0400

    Add assert_value

--- mix.exs
+++ mix.exs
@@ -22,6 +22,7 @@ defmodule Chex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:assert_value, "~> 0.9.3"},
       {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
