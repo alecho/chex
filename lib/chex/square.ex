90fed844513ff9d0b5888d82e5e3130b079d58c1
Andrew Lechowicz <andrew@alecho.com>
2019-09-12 15:45:07 -0400

    Add color method

--- lib/chex/square.ex
+++ lib/chex/square.ex
@@ -1,6 +1,16 @@
 defmodule Chex.Square do
+  require Integer
+
   @typedoc """
   File a-h as an atom, with an file integer 1-8.
   """
   @type t :: {file :: atom, rank :: pos_integer}
+
+  def color({file, rank}) when file in [:a, :c, :e, :g] and Integer.is_even(rank), do: :light
+
+  def color({file, rank}) when file in [:a, :c, :e, :g] and Integer.is_odd(rank), do: :dark
+
+  def color({file, rank}) when file in [:b, :d, :f, :h] and Integer.is_even(rank), do: :dark
+
+  def color({file, rank}) when file in [:b, :d, :f, :h] and Integer.is_odd(rank), do: :light
 end
---
+++ test/chex/square_test.exs
@@ -0,0 +1,81 @@
+defmodule Chex.SquareTest do
+  use ExUnit.Case
+  doctest Chex.Square
+
+  test "correctly determines color" do
+    squares = for f <- [:a, :b, :c, :d, :e, :f, :g, :h], r <- 1..8, do: {f, r}
+
+    results =
+      squares
+      |> Enum.map(fn sq ->
+        {sq, Chex.Square.color(sq)}
+      end)
+
+    assert results == [
+             {{:a, 1}, :dark},
+             {{:a, 2}, :light},
+             {{:a, 3}, :dark},
+             {{:a, 4}, :light},
+             {{:a, 5}, :dark},
+             {{:a, 6}, :light},
+             {{:a, 7}, :dark},
+             {{:a, 8}, :light},
+             {{:b, 1}, :light},
+             {{:b, 2}, :dark},
+             {{:b, 3}, :light},
+             {{:b, 4}, :dark},
+             {{:b, 5}, :light},
+             {{:b, 6}, :dark},
+             {{:b, 7}, :light},
+             {{:b, 8}, :dark},
+             {{:c, 1}, :dark},
+             {{:c, 2}, :light},
+             {{:c, 3}, :dark},
+             {{:c, 4}, :light},
+             {{:c, 5}, :dark},
+             {{:c, 6}, :light},
+             {{:c, 7}, :dark},
+             {{:c, 8}, :light},
+             {{:d, 1}, :light},
+             {{:d, 2}, :dark},
+             {{:d, 3}, :light},
+             {{:d, 4}, :dark},
+             {{:d, 5}, :light},
+             {{:d, 6}, :dark},
+             {{:d, 7}, :light},
+             {{:d, 8}, :dark},
+             {{:e, 1}, :dark},
+             {{:e, 2}, :light},
+             {{:e, 3}, :dark},
+             {{:e, 4}, :light},
+             {{:e, 5}, :dark},
+             {{:e, 6}, :light},
+             {{:e, 7}, :dark},
+             {{:e, 8}, :light},
+             {{:f, 1}, :light},
+             {{:f, 2}, :dark},
+             {{:f, 3}, :light},
+             {{:f, 4}, :dark},
+             {{:f, 5}, :light},
+             {{:f, 6}, :dark},
+             {{:f, 7}, :light},
+             {{:f, 8}, :dark},
+             {{:g, 1}, :dark},
+             {{:g, 2}, :light},
+             {{:g, 3}, :dark},
+             {{:g, 4}, :light},
+             {{:g, 5}, :dark},
+             {{:g, 6}, :light},
+             {{:g, 7}, :dark},
+             {{:g, 8}, :light},
+             {{:h, 1}, :light},
+             {{:h, 2}, :dark},
+             {{:h, 3}, :light},
+             {{:h, 4}, :dark},
+             {{:h, 5}, :light},
+             {{:h, 6}, :dark},
+             {{:h, 7}, :light},
+             {{:h, 8}, :dark}
+           ]
+  end
+end
