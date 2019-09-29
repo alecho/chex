aafe7f377c6c50ad2081297d5217fcf2d3f4e031
Andrew Lechowicz <andrew@alecho.com>
2019-09-29 13:23:13 -0400

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Chex.MixProject do
   def project do
     [
       app: :chex,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       description: description(),
