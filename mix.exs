7272c8d0ff14513bc4740298b93012cc9994800f
Andrew Lechowicz <andrew@alecho.com>
2019-09-30 13:52:38 -0400

    Version to 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Chex.MixProject do
   def project do
     [
       app: :chex,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       description: description(),
