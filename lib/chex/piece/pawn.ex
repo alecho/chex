2c4a4b182d86ff7ffa84b9fdfc16661098268bdd
Andrew Lechowicz <andrew@alecho.com>
2019-10-28 13:30:39 -0400

    Add pawn movement

--- lib/chex/piece/pawn.ex
+++ lib/chex/piece/pawn.ex
@@ -1,8 +1,15 @@
 defmodule Chex.Piece.Pawn do
-  alias Chex.{Board, Square}
   @behaviour Chex.Piece
 
-  def possible_moves(_color, _square = {_file, _rank}, _game), do: []
+  def possible_moves(color, square, _game) do
+    moves(color, square)
+  end
 
-  def attacking_squares(_color, _square = {_file, _rank}, _ep), do: []
+  def attacking_squares(_color, _square = {_file, _rank}, _ep \\ nil),
+    do: []
+
+  defp moves(:white, {file, 2}), do: [{file, 3}, {file, 4}]
+  defp moves(:white, {file, rank}), do: [{file, rank + 1}]
+  defp moves(:black, {file, 7}), do: [{file, 6}, {file, 5}]
+  defp moves(:black, {file, rank}), do: [{file, rank - 1}]
 end
