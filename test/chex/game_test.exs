1a70e44ea715e742c3f818a9bfbfbe2448ba7e4b
Andrew Lechowicz <andrew@alecho.com>
2020-10-10 23:06:31 -0400

    Move in_check/2 tests to Chex.Checktest

--- test/chex/game_test.exs
+++ test/chex/game_test.exs
@@ -241,23 +241,6 @@ defmodule Chex.GameTest do
     end
   end
 
-  describe "in_check?/2" do
-    test "not in check is false" do
-      {:ok, game} = Game.new()
-      assert Game.in_check?(game, :black) == false
-    end
-
-    test "in check is true" do
-      {:ok, game} = Game.new("4k3/8/8/8/4R3/8/8/4K3 w - - 0 1")
-      assert Game.in_check?(game, :black) == true
-    end
-
-    test "in mate is true" do
-      {:ok, game} = Game.new("4k2Q/R7/8/8/8/8/8/4K3 w - - 0 1")
-      assert Game.in_check?(game, :black) == true
-    end
-  end
-
   describe "castling" do
     test "white kingside castle" do
       {:ok, game} = Game.new("r3k2r/8/8/8/8/8/8/R3K2R w KQkq - 0 1")
